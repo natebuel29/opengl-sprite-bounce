0ea462c6b31d800ee8878c0f9db14fc667ac64d2
Nate Buel <natebuel29@gmail.com>
2023-10-15 17:32:11 -0500

    9/15 - work on linking glfw via cmake

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,38 @@
 
 int main()
 {
+
+    GLFWwindow *window;
+
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
     std::cout << "Init cmake and glfw" << std::endl;
 
     return 0;
