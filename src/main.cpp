3699b105f235bdaed61a6980fb61ef7ad3f49fed
Nate Buel <natebuel29@gmail.com>
2023-10-15 20:12:38 -0500

    10/15 - begin writing gl initialize function and shader

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
-#include <GLFW/glfw3.h>
 #include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// #############################################################################
+//                           Globals
+// #############################################################################
+
+// #############################################################################
+//                           Structs
+// #############################################################################
+
+struct OpenGLContext
+{
+};
+
+// #############################################################################
+//                           Functions
+// #############################################################################
+void glInit(OpenGLContext *glContext);
+
+void window_size_callback(GLFWwindow *window, int width, int height);
 
 int main()
 {
@@ -9,8 +28,10 @@ int main()
 
     if (!glfwInit())
         return -1;
-
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    window = glfwCreateWindow(768, 768, "Sprite Bounce", NULL, NULL);
 
     if (!window)
     {
@@ -20,6 +41,18 @@ int main()
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    gladLoadGL();
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 768, 768);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -39,4 +72,29 @@ int main()
     std::cout << "Init cmake and glfw" << std::endl;
 
     return 0;
+}
+
+void glInit(OpenGLContext *glContext)
+{
+    float verticies[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f};
+
+    GLuint VBO, VAO;
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_DYNAMIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+}
+
+void window_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
